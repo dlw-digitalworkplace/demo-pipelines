4d2b2791445ac37a35aada484cf6f59dfe74aee9
Pieter Heemeryck <pieter.heemeryck@delaware.pro>
2021-08-17 11:00:15 +0200

    Added .NET Framework web app

---
+++ server/NetFramework/Pipelines.NetFramework/Pipelines.WebApp/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Pipelines.WebApp.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
