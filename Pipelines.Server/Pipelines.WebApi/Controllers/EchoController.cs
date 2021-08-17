3a797a01bf53b892798de54d5ae2d237b1da7df8
Pieter Heemeryck <pieter.heemeryck@delaware.pro>
2021-08-17 09:38:50 +0200

    Added .NET core web api

---
+++ Pipelines.Server/Pipelines.WebApi/Controllers/EchoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Pipelines.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EchoController : ControllerBase
+    {
+        private readonly ILogger<EchoController> _logger;
+
+        public EchoController(ILogger<EchoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get(string shout)
+        {
+            return shout;
+        }
+    }
+}
