583d0a2a50d28bb5076428e5ac77e50c7610e0df
Pieter Heemeryck <pieter.heemeryck@delaware.pro>
2021-10-21 14:24:14 +0200

    change to front end

--- client/pipeline-webpart/src/webparts/pipeline/components/Pipeline.tsx
+++ client/pipeline-webpart/src/webparts/pipeline/components/Pipeline.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import styles from './Pipeline.module.scss';
-import { IPipelineProps } from './IPipelineProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import styles from "./Pipeline.module.scss";
+import { IPipelineProps } from "./IPipelineProps";
+import { escape } from "@microsoft/sp-lodash-subset";
 
 export default class Pipeline extends React.Component<IPipelineProps, {}> {
   public render(): React.ReactElement<IPipelineProps> {
@@ -10,9 +10,13 @@ export default class Pipeline extends React.Component<IPipelineProps, {}> {
         <div className={styles.container}>
           <div className={styles.row}>
             <div className={styles.column}>
-              <span className={styles.title}>Pipeline web part - {process.env.SPFX_MY_ENV}</span>
+              <span className={styles.title}>
+                Pipeline web part CHANGE - {process.env.SPFX_MY_ENV}
+              </span>
               <p className={styles.subTitle}>Make pipelines great again!</p>
-              <p className={styles.description}>{escape(this.props.description)}</p>
+              <p className={styles.description}>
+                {escape(this.props.description)}
+              </p>
               <a href="https://aka.ms/spfx" className={styles.button}>
                 <span className={styles.label}>Learn more</span>
               </a>
